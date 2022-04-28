5c7aea23e0e5bbee3a5147fee137ebd3a95c2db6
Matheus Santos <mat.re.oliveira@gmail.com>
2022-04-28 16:15:01 -0400

    Ajustes de design

--- src/views/Caracteristicas/CadastroCaracteristica.vue
+++ src/views/Caracteristicas/CadastroCaracteristica.vue
@@ -9,39 +9,26 @@
     <div class="row">
       <div class="col-12">
         <b-form >
-          <b-form-group
-            id="fieldset"           
-            label="Informe a Categoria"
-            label-for="categoria">    
-            <b-form-input
-              id="categoria" v-model="categoria"            
-            ></b-form-input>
+          <b-form-group id="fieldset" label="Informe o nome da categoria" label-for="categoria">    
+            <b-form-input id="categoria" v-model="categoria" />
           </b-form-group>
 
-          <b-form-group
-            id="fieldset2" label="Informe o(s) tipo(s) da categoria"          
-            label-for="tipo"> 
-          
-            <b-form-input
-              id="tipo" v-model="tipos[0]"                    
-            ></b-form-input>   
-            
+          <b-form-group id="fieldset2" label="Informe o(s) tipo(s) da categoria (adicione mais campos se achar necessário)" label-for="tipo"> 
             <div class="py-2" >
-                <b-button class="mr-2" variant="primary" @click="quantidadeInput('add')">+</b-button>
-                <b-button variant="danger" @click="quantidadeInput('remove')" v-show="qtd_inputs>0">-</b-button>
+                <b-button class="mr-2" variant="primary" @click="quantidadeInput('add')">
+                  Adicionar
+                </b-button>
+                <b-button variant="danger" @click="quantidadeInput('remove')" v-show="qtd_inputs>0">
+                  Remover
+                </b-button>
             </div>
+            <b-form-input class="mb-2" v-model="tipos[0]"  :placeholder="'Tipo 1'" />  
+            <b-form-input class="mb-2" v-model="tipos[i+1]" :placeholder="'Tipo '+(i+2) " v-for="(qtd, i) in qtd_inputs" :key="qtd" />
+          </b-form-group>     
 
-          <b-form-input class="mb-2"
-              id="tipo" v-model="tipos[i+1]"    
-              v-for="(qtd, i) in qtd_inputs" :key="qtd"            
-            ></b-form-input>
-
-        </b-form-group>     
-
-        <div class="text-center">
-            <b-button variant="success">Salvar</b-button>
-        </div>      
-
+          <div class="text-center">
+              <b-button variant="success">Salvar</b-button>
+          </div>
         </b-form>
       </div>
     </div>
@@ -66,18 +53,15 @@ export default Vue.extend({
   },
   methods: {
       quantidadeInput(acao: string) {
-
-          if(acao == 'add') {
-            this.qtd_inputs++
+        if(acao == 'add') {
+          this.qtd_inputs++
+        }
+        if(acao == 'remove') {
+          if(this.qtd_inputs > 0 ){
+              this.qtd_inputs--
           }
-          if(acao == 'remove') {
-            
-            if(this.qtd_inputs > 0 ){
-                this.qtd_inputs--
-            }
-            
-          }
+        }
       }
   }
-});
+})
 </script>
