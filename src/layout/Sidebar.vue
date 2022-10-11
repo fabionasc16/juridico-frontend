b6027b394e1d66db8c593b9802121a6efd6d4907
Ingrid Cavalcanti <ingridcxavier@gmail.com>
2022-10-11 10:20:48 -0400

    menu cadastro

--- src/layout/Sidebar.vue
+++ src/layout/Sidebar.vue
@@ -6,11 +6,28 @@
         <!-- <h3>Sistema de Acompanhamento dos Processos Judiciais</h3> -->
         </div>
         <!-- Listagem das rotas -->
-        <nav class="listagem pt-5">
-            <router-link class="d-block p-2" v-for="(r,index) in commonMenus" :key="index" :to="r.path">
-                <p style="font-weight: 600;">{{r.name}}</p>
-            </router-link>
+            <nav class="listagem pt-5">
+                <router-link class="d-block p-2" v-for="(r,index) in commonMenus" :key="index" :to="r.path" 
+                v-if="r.meta.menu!='Cadastros'">
+                    <p style="font-weight: 600;">{{r.name}}</p> 
+                </router-link>
         </nav>
+
+            <b-dropdown class="dropdown" size="sm" variant="outline-light">
+                    <template #button-content>
+                        <span>
+                         Cadastros
+                        </span>
+                    </template>
+
+                <b-dropdown-item v-for="(r,index) in commonMenus" :key="index" v-if="r.meta.menu=='Cadastros'"> 
+                    <router-link class="d-block p-2" :to="r.path" style="margin-bottom:-20px;" >
+                         <label style="font-weight: 600;">{{r.name}}</label> 
+                        </router-link>
+                </b-dropdown-item>
+            </b-dropdown>          
+
+        
         <div class="footer">
             <p class="justify-center" style="color: white; font-weight: 600;">
                 Secretaria de Saúde 
