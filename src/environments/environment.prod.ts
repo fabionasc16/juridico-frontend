9c96c65a024198ee24a02d43fa4a0129beda800c
Marcelo Pinheiro da Silva <marcelopds70@gmail.com>
2022-10-14 19:50:01 +0000

    Update src/environments/environment.prod.ts, docker-compose.yml, tsconfig.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export let environment = {
   production: true,
+  NODE_ENV: "production",
   apiURL: "https://sistemas.saude.am.gov.br/sapej/api",  
   apiURLSSO: "https://sistemas.saude.am.gov.br/sapej/api/v1"
 
